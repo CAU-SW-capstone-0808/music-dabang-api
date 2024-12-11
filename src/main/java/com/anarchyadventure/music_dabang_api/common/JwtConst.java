5c58d555ba23ff8f699348ccdcc62ad56e212767
Jaden Choi <jkya02@gmail.com>
2024-12-12 02:24:25 +0900

    JWT: 유효 기간 1일 설정

--- src/main/java/com/anarchyadventure/music_dabang_api/common/JwtConst.java
+++ src/main/java/com/anarchyadventure/music_dabang_api/common/JwtConst.java
@@ -6,7 +6,7 @@ package com.anarchyadventure.music_dabang_api.common;
 public class JwtConst {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
-    public static final long ACCESS_TOKEN_EXPIRE = 30 * 60L; // 30분
+    public static final long ACCESS_TOKEN_EXPIRE = 24 * 60 * 60L; // 1일
 //    public static final long ACCESS_TOKEN_EXPIRE = 10L; // 10초: 테스트용
     public static final long ANONYMOUS_TOKEN_EXPIRE = 30L * 60L; // 30분
     public static final long REFRESH_TOKEN_EXPIRE = 14 * 24 * 60 * 60L; // 14일
